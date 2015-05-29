84538836ef43312854cb398970450e06edb1062b
MegaApuTurkUltra <apu@aputurk.tk>
2015-05-28 20:12:22 -0400

    More visualizer updates

--- src/apu/scratch/tools/gen/visualizer/VisualizerGenMinim.java
+++ src/apu/scratch/tools/gen/visualizer/VisualizerGenMinim.java
@@ -42,7 +42,8 @@ public class VisualizerGenMinim extends ToolBase {
 		System.out.println("Initializing...");
 		File input = new File(args[0]);
 		File output = new File(args[1]);
-		boolean bass_only = !args[2].equals("true");
+		int fps = args[2].equals("true") ? 20 : 10;
+		boolean bass_only = !args[3].equals("true");
 
 		ZipOutputStream outFile = new ZipOutputStream(new FileOutputStream(
 				output));
@@ -62,7 +63,7 @@ public class VisualizerGenMinim extends ToolBase {
 		FFT fft = new FFT(256 * 64, sampleRate);
 		fft.linAverages(30);
 
-		float sampleRateFrames = sampleRate / 10;
+		float sampleRateFrames = sampleRate / fps;
 
 		int counter = 0;
 		int percent = 0;
@@ -164,8 +165,12 @@ public class VisualizerGenMinim extends ToolBase {
 				new ParamDef("output", "The sb2 file to write to", false,
 						ParamType.FILE_OUT),
 				new ParamDef(
+						"highFps",
+						"True to generate animation frames at 20 FPS (warning: file will be massive), false to generate it at 10 (recommended)",
+						false, ParamType.BOOLEAN),
+				new ParamDef(
 						"fullSpectrum",
-						"True to analyze a large spectrum of frequencies, false or omit for bass only",
+						"True to analyze a large spectrum of frequencies, false for bass only",
 						false, ParamType.BOOLEAN) };
 	}
 
